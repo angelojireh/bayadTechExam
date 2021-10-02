a818535212026acffeb50825a031f52ee269f00a
angelo <angelojirehfidel@gmail.com>
2021-10-02 10:25:35 +0800

    code wrap changes

--- app/src/main/java/com/example/bayadtechexam/models/PromoRepo.kt
+++ app/src/main/java/com/example/bayadtechexam/models/PromoRepo.kt
@@ -13,16 +13,15 @@ class PromoRepo {
     private var apiInterface:ApiInterface = ApiClient.getApiClient().create(ApiInterface::class.java)
 
     fun fetchAllPromos():LiveData<List<PromoModel>>{
+
         val data = MutableLiveData<List<PromoModel>>()
+
         apiInterface.fetchAllPromos().enqueue(object: Callback<List<PromoModel>>{
             override fun onFailure(call: Call<List<PromoModel>>, t: Throwable) {
                 data.value = null
             }
 
-            override fun onResponse(
-                call: Call<List<PromoModel>>,
-                response: Response<List<PromoModel>>
-            ) {
+            override fun onResponse(call: Call<List<PromoModel>>, response: Response<List<PromoModel>>) {
                 val res = response.body()
                 if(response.code() == HTTP_OK && res!=null){
                     data.value = res
